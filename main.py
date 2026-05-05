64aafb2f325a4ec10ae7d21a0f615fbcfa2bd87f
deirks2 <dogun0406@gmail.com>
2026-05-05 10:11:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import discord
 from discord.ext import commands
 import google.generativeai as genai
-import google.generativeai as genai
+from google.generativeai import types
 from dotenv import load_dotenv
 from collections import defaultdict
 
